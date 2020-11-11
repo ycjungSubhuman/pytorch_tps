9445af8af68332e081c52c6ee1086d653cc7bc6e
hyominkim <min00001@postech.ac.kr>
2020-11-11 19:14:25 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pytorch_tps',
+    description='Thin plate spline interpolation for PyTorch',
+    version="0.0.1",
+    author='Yucheol Jung',
+    author_email='ycjung@postech.ac.kr',
+    packages=['pytorch_tps'],
+    url='https://github.com/ycjungSubhuman/pytorch_tps',
+)
